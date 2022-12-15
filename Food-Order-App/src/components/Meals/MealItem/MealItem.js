1598de5bef54fc1b7c73aa8c482d80972a61d806
vasil.krumov <vassil.krumov@gmail.com>
2022-12-15 20:42:32 +0200

    🥘 Food Order App Initial Commit

---
+++ Food-Order-App/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './MealItem.module.css'
+import { MealItemForm } from './MealItemForm'
+
+export const MealItem = ({ name, description, price }) => {
+    const formattedPrice = `$${price.toFixed(2)}`
+    return (
+        <li className={classes.meal}>
+            <div>
+                <h3>{name}</h3>
+                <div className={classes.description}>{description}</div>
+                <div className={classes.price}>{formattedPrice}</div>
+            </div>
+            <div>
+                <MealItemForm />
+            </div>
+        </li>
+    )
+}
