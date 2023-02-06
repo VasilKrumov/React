6581f4bbd1a21de7f065cd8c1f43ebad098488d4
vasil.krumov <vassil.krumov@gmail.com>
2023-02-06 11:45:21 +0200

    🎉 Module Complete

--- Behind-The-Scenes/src/App.js
+++ Behind-The-Scenes/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import Button from './components/UI/Button/Button'
 
 import './App.css'
@@ -6,13 +6,22 @@ import DemoOutput from './components/Demo/DemoOutput'
 
 function App() {
     const [showParagraph, setShowParagrah] = useState(false)
-    const toggleParagraphHandler = () => {
-        setShowParagrah((prevShowParagraph) => !prevShowParagraph)
+    const [allowToggle, setAllowToggle] = useState(false)
+    const toggleParagraphHandler = useCallback(() => {
+        if (allowToggle) {
+            setShowParagrah((prevShowParagraph) => !prevShowParagraph)
+        }
+    }, [allowToggle])
+
+    const allowToggleHandler = () => {
+        setAllowToggle(true)
     }
+    console.log('app')
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
+            <Button onClick={allowToggleHandler}>Allow Toggle</Button>
             <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
             {showParagraph && <DemoOutput show={showParagraph} />}
         </div>
--- Behind-The-Scenes/src/components/Demo/DemoOutput.js
+++ Behind-The-Scenes/src/components/Demo/DemoOutput.js
@@ -1,3 +1,8 @@
-export default function DemoOutput(props) {
+import React from 'react'
+
+const DemoOutput = (props) => {
+    console.log('demo output')
     return <p>{props.show ? 'this is new!' : ''}</p>
 }
+
+export default React.memo(DemoOutput)
